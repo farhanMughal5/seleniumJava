8645be968b84b0c6c04d8be2e5cc3fb68b974fa7
farhanmughal5 <farhanmughal531@gmail.com>
2024-11-07 11:53:15 +0500

    Selenium Java Project Pushed for the first time

---
+++ SimpleSeleniumProject/simpleAutomation.java
@@ -0,0 +1,36 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class simpleAutomation {
+
+    public static void main(String[] args) {
+    	String driverPath = "geckodriver.exe";
+        // Set the path to the ChromeDriver executable
+        System.setProperty("webdriver.chrome.driver", driverPath);
+
+        // Instantiate ChromeDriver
+        WebDriver driver = new ChromeDriver();
+
+        // Maximize browser window
+        driver.manage().window().maximize();
+
+        // Navigate to Google
+        driver.get("https://www.google.com");
+
+        // Find the search input field using the name locator and type "Selenium WebDriver"
+        WebElement searchBox = driver.findElement(By.name("q"));
+        searchBox.sendKeys("Selenium WebDriver");
+
+        // Submit the search form
+        searchBox.submit();
+
+        // Wait for the results to load and click the first result (link locator)
+        WebElement firstResult = driver.findElement(By.xpath("//h3"));
+        firstResult.click();
+
+        // Close the browser
+        driver.quit();
+    }
+}
